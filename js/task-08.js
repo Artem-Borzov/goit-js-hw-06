02999cf606f5fd8b0372c5fe2f82e4e6f17310ec
Artem-Borzov <abug990@gmail.com>
2022-09-26 17:15:29 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,14 @@
+const formEl = document.querySelector(".login-form");
 
+formEl.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const email = event.currentTarget.elements.email;
+  const password = event.currentTarget.elements.password;
+  if (email.value === "" || password.value === "") {
+    return alert("Заполните пустые поля!");
+  }
+  console.log({ email: email.value, password: password.value });
+  formEl.reset();
+}
