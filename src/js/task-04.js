cdf9c75d1b902b3fcbcde122d680395e8c1c80a5
Artem-Borzov <abug990@gmail.com>
2022-09-26 11:27:23 +0300

    Update task-04.js

---
+++ src/js/task-04.js
@@ -0,0 +1,15 @@
+let counterValue = 0;
+const decrement = document.querySelector('[data-action="decrement"]');
+const increment = document.querySelector('[data-action="increment"]');
+const counter = document.querySelector("#value");
+
+decrement.addEventListener("click", () => {
+  counterValue -= 1;
+  console.log(counterValue);
+  counter.textContent = counterValue;
+});
+increment.addEventListener("click", () => {
+  counterValue += 1;
+  console.log(counterValue);
+  counter.textContent = counterValue;
+});
